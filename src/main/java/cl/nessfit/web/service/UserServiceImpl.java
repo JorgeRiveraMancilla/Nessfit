08254001f48cc39022dc849c218039b66d1daadd
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2022-09-22 20:57:10 -0300

    fix: Missing tag in UserServiceImpl class

--- src/main/java/cl/nessfit/web/service/UserServiceImpl.java
+++ src/main/java/cl/nessfit/web/service/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package cl.nessfit.web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import cl.nessfit.web.model.User;
 import cl.nessfit.web.repository.UserRepositoryInterface;
 
+@Service
 public class UserServiceImpl implements UserServiceInterface {
     @Autowired
     private UserRepositoryInterface userRepositoryInterface;
