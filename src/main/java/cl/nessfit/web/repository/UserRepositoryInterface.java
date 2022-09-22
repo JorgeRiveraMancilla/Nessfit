ad1f69de83ee0ddb9a6085614b2e022c406652ff
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2022-09-22 20:47:58 -0300

    feat: New UserRepositoryInterface

---
+++ src/main/java/cl/nessfit/web/repository/UserRepositoryInterface.java
@@ -0,0 +1,9 @@
+package cl.nessfit.web.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cl.nessfit.web.model.User;
+
+public interface UserRepositoryInterface extends JpaRepository<User, String> {
+    
+}
