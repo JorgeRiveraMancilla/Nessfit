3825f3f62a2fc1899fca672a7613637e86c0a40f
Zenkaizer <m.cespedes.arq@gmail.com>
2022-09-24 13:37:13 -0300

    fix: Password is now encrypted

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ INSERT INTO `nessfit`.roles (id, name) VALUES (2, 'ADMINISTRATIVO');
 INSERT INTO `nessfit`.roles (id, name) VALUES (3, 'CLIENTE');
 
 INSERT INTO users (rut, email, first_name, last_name, password, phone, status, id_role)
-VALUES ('17977139K', 'antonio.barraza.guzman@gmail.com', 'Antonio', 'Barraza', '{noop}Nessfit_$22', -1, 1, 1);
+VALUES ('17977139K', 'antonio.barraza.guzman@gmail.com', 'Antonio', 'Barraza', '$2a$10$sBHSQ4CahXOCjbuyNyDu7e8VAcB5Exaq6v7brxSCGlJiV52MK6X2u', -1, 1, 1);
