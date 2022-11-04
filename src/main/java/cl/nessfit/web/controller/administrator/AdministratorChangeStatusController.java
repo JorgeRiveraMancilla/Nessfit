385302030621bacb41700bdf949eb3c1c9a2ef12
Zenkaizer <m.cespedes.arq@gmail.com>
2022-11-04 16:42:33 -0300

    fix: ChangeStatus fixed

--- src/main/java/cl/nessfit/web/controller/administrator/AdministratorChangeStatusController.java
+++ src/main/java/cl/nessfit/web/controller/administrator/AdministratorChangeStatusController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.Map;
@@ -25,47 +22,21 @@ public class AdministratorChangeStatusController {
      */
     @GetMapping("/change-status-user")
     public String changeStatusUser() {
-        return "change-status-user";
+        return "administrator/manage-user";
     }
 
     /**
      * Changes the user status, depending on the form election by the administrator or administrative.
-     * @param allRequestParams form data.
-     * @param model is the application's dynamic data structure.
+     * @param rut
      * @return return to "change-status-user" page, or home page.
      */
-    @PostMapping("/change-status-user")
-    public String changeStatusUser(@RequestParam Map<String,String> allRequestParams, Model model) {
-        String rut = allRequestParams.get("rut");
-        String status = allRequestParams.get("status");
+    @PostMapping("/change-status-user/{rut}")
+    public String changeStatusUser(@PathVariable String rut) {
+
         User user = userService.searchByRut(rut);
 
-        if (status == null) {
-            User userLogged = userService.searchByRut(SecurityContextHolder.getContext().getAuthentication().getName());
-            int idUserLogged = userLogged.getRole().getId();
-
-            if (user == null) {
-                model.addAttribute("msgRutExist", false);
-            } else {
-                int idUser = user.getRole().getId();
-                if (idUser <= idUserLogged) {
-                    if (userLogged.getRole().getId() == 1) {
-                        model.addAttribute("msgChangeAdministrator", false);
-                    } else {
-                        model.addAttribute("msgChangeAdministrative", false);
-                    }
-                } else {
-                    model.addAttribute("msgChange", "");
-                    model.addAttribute("msgRut", rut);
-                    model.addAttribute("msgName", user.getFirstName());
-                    model.addAttribute("msgStatus", user.getStatus() == 0 ? "Deshabilitado" : "Habilitado");
-                }
-            }
-            return "change-status-user";
-        } else {
-            user.setStatus(status.equals("Habilitado") ? 0 : 1);
-            userService.save(user);
-            return "redirect:/";
-        }
+        user.setStatus(user.getStatus() == 1 ? 0 : 1);
+        userService.save(user);
+        return "redirect:/administrator/manage-user";
     }
 }
