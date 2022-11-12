d2621eda46ce0eb1a0ec21119e86727d1f484168
Rey Valdes <reyvaldesm@gmail.com>
2022-11-12 20:41:32 -0300

    feat: refresh button for crud without functionality

---
+++ src/test/java/cl/nessfit/web/util/ProfileValidationTest.java
@@ -0,0 +1,19 @@
+package cl.nessfit.web.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ProfileValidationTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
